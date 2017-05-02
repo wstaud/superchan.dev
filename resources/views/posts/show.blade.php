934aca52bfd09a2ff9dda17977838177759ea822
xconstaud <xconstaud@gmail.com>
2017-05-02 00:51:50 -0500

    Update show style

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,16 +2,20 @@
 
 
 @section('content')
-<div class="row align-center align-middle">
-    <div class="card" style="width: 300px;">
-        <img src="http://images.phpgang.com/2015/11/Laravel-5.png">
-        <div class="card-section">
-            <h4>{{$post->title}}</h4>
-            <p>{{$post->content}}</p>
-            <a href="{{$post->url}}">{{$post->url}}</a>
-      <p class="timeFont">Created on: {{$post->created_at->diffForHumans()}} by:</p>
-        </div>
-    </div>
+<div class="container-fluid marginTop">
+	<div class="row">
+	    <div class="col-sm-12 text-center">
+	        <a href="https://images7.alphacoders.com/344/344627.jpg"><img class="img-fluid contentPhoto" src="https://images7.alphacoders.com/344/344627.jpg"></a>
+	    </div>
+	</div>
+	<div class="row marginTop">
+		<div class="col-sm-12">
+			<h4>{{$post->title}}</h4>
+		    <p>{{$post->content}}</p>
+		    <a href="{{$post->url}}">{{$post->url}}</a>
+			<p class="timeFont">Created on: {{$post->created_at->diffForHumans()}} by:</p>
+		</div>
+	</div>
 </div>
 
 @stop
